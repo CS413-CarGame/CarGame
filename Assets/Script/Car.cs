f54874b70600dba8abee0e5c6e8e1206ecc4b791
Joshua <jcv234@nau.edu>
2023-10-13 00:55:23 -0700

    Adding HP Bar and Damage from Zombies

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -3,25 +3,80 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
     // Start is called before the first frame update
     static public Vector3 POS = Vector3.zero;
     static public Vector3 TRUCKVEL = Vector3.zero;
+    public HealthBar healthBar; 
+    public int ZombDamage = 7;
+    public int WallDamageMult = 10;
+    public int HEALTH;
+    public int maxHealth = 100;
+
     Rigidbody rb;
 
     void Start()
     {
+        HEALTH = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
         rb = GetComponent<Rigidbody>();
     }
 
-    
+    void Update()
+    {
+        if(CheckForDeath(HEALTH))
+        {
+            LoadMainGame();
+        }
+    }
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("_Scene_1");
+    }
+
     private void FixedUpdate()
     {
         UnityEngine.Debug.Log(rb.velocity.magnitude);
         TRUCKVEL = rb.velocity;
         Vector3 tPos = transform.position;
         POS = tPos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zomb"))
+        {
+            if(TRUCKVEL.magnitude <= 4.5)
+            {
+                TakeDamage(ZombDamage);
+            }
+        }
+        if (other.gameObject.CompareTag("Arena"))
+        {
+            if (TRUCKVEL.magnitude >= 3.5)
+            {
+                TakeDamage(WallDamageMult);
+            }
+        }
+
+    }
+
+    bool CheckForDeath(int health)
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void TakeDamage(int damage)
+    {
+        HEALTH -= damage;
+        healthBar.SetHealth(HEALTH);
     }
 }
