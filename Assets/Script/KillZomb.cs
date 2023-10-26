8122e07e7c196fb9a618265d50b8e805843bf39e
Max Poole <mkp274@nau.edu>
2023-10-26 08:26:54 -0700

    Added zombie noises

--- Assets/Script/KillZomb.cs
+++ Assets/Script/KillZomb.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KillZomb : MonoBehaviour
-{   
+{
+    public AudioSource audioPlayer;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Zomb"))
@@ -13,6 +15,8 @@ public class KillZomb : MonoBehaviour
                 Destroy(other.gameObject);
 
                 ScoreManager.instance.AddScore(100);
+
+                audioPlayer.Play();
             }
         }
     }
