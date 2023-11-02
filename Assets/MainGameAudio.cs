1cee945328798f68a1bb734f7017efde87471a67
Max Poole <mkp274@nau.edu>
2023-11-02 09:59:39 -0700

    Menu and Main Music. Main Music to be changed later but thats a simple file change

---
+++ Assets/MainGameAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameAudio : MonoBehaviour
+{
+
+    public AudioSource audioPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
