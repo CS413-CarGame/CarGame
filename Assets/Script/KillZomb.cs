a5eb42381246c214b15f989a3b6f1e3498f5f830
Joshua <jcv234@nau.edu>
2023-11-09 16:02:03 -0700

    Death Particles

--- Assets/Script/KillZomb.cs
+++ Assets/Script/KillZomb.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class KillZomb : MonoBehaviour
 {
     public AudioSource audioPlayer;
+    public ParticleSystem deathEffect;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +13,8 @@ public class KillZomb : MonoBehaviour
         {
             if(Car.TRUCKVEL.magnitude >= 4.5)
             {
+                Instantiate(deathEffect, transform.position, Quaternion.identity);
+
                 Destroy(other.gameObject);
 
                 ScoreManager.instance.AddScore(100);
