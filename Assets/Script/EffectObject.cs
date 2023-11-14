db11596ece3d0465ba8b5082a020a9a0a2c11e66
Josh VanderMeer <Lobojoshvandermeer@gmail.com>
2023-11-14 09:37:29 -0700

    fix max stuff

---
+++ Assets/Script/EffectObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectObject : MonoBehaviour
+{
+    [Header("Unity Setup")]
+    public float time = 5;
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
