b0759304da1ddd3c0b305863f785325dbc25a215
APoole11 <acp334@nau.edu>
2023-11-20 21:14:19 -0700

    Speedy boy implementation

---
+++ Assets/Script/SpeedometerManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour {
+
+    private const float MAX_SPEED_ANGLE = -20;
+    private const float ZERO_SPEED_ANGLE = 230;
+    private Transform needleTransform;
+    private float speedMax;
+    private float speed;
+
+    private void Awake() {
+        needleTransform = transform.Find("Needle");
+
+        speed = 0f;
+        speedMax = 200f;
+    }
+
+    private void Update() {
+        speed = Car.TRUCKVEL.magnitude*5;
+
+        needleTransform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
+    }
+
+    private float GetSpeedRotation() {
+        float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
+
+        float speedNormalized = speed / speedMax;
+
+        return ZERO_SPEED_ANGLE - speedNormalized * totalAngleSize;
+    }
+}
