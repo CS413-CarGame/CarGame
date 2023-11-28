0034cba6bfd2cd69a31806bd880cc70db0c9515f
Caden Tedeschi <cmt489@nau.edu>
2023-11-28 13:08:50 -0700

    Powerups spin and hover

---
+++ Assets/Script/PowerUpRotate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class PowerUpRotate : MonoBehaviour
+{
+    public float rotationsPerSecond = 0.1f;
+    public float speed = 5f;
+    public float height = 0.5f;
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        float rY = -(rotationsPerSecond * Time.time * 360) % 360f;
+        
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        
+        transform.position = new Vector3(pos.x, newY, pos.z);
+        
+        transform.rotation = Quaternion.Euler(0, rY, 0);
+    }
+}
